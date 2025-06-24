0862db93b3f94782af08ba14b4df4aec7b4a9afd
AayushAnand39 <aayushanand737@gmail.com>
2025-06-24 12:01:31 +0530

    Removed consumers.py and routing.py

--- pollsapp/pollsapp/asgi.py
+++ pollsapp/pollsapp/asgi.py
@@ -11,13 +11,9 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import polls_app.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pollsapp.settings')
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
-    URLRouter(polls_app.routing.websocket_urlpatterns)
-  ),
 })
